38902b38333c3f0fe29452f907ee4fd4e727e40f
Batool Madi <batoolsawal@gmail.com>
2026-03-16 01:20:49 +0300

    trigger autograder

--- test_drill.py
+++ test_drill.py
@@ -20,8 +20,10 @@ def test_clean_column():
 def test_compute_revenue():
     quantity = pd.Series([1, 2, 3])
     price = pd.Series([10, 20, 30])
-    
+
     result = compute_revenue(quantity, price)
 
     expected = pd.Series([10, 40, 90])
     assert result.equals(expected)
+
+# trigger autograder
